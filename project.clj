2b453a9ca59f1069dc4e7a5bbdd866dbeeaa6091
Thorsten Ludwig <tludwig@inovex.de>
2018-02-22 13:22:04 +0100

    Add dependencies

--- project.clj
+++ project.clj
@@ -5,11 +5,17 @@
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [com.h2database/h2 "1.4.196"]
                  [compojure "1.5.1"]
+                 [digest "1.4.6"]
                  [liberator "0.15.1"]
+                 [ring/ring-core "1.6.2"]
                  [ring/ring-defaults "0.2.1"]
+                 [ring/ring-json "0.4.0"]
                  [yesql "0.5.3"]]
   :plugins [[lein-ring "0.9.7"]]
   :ring {:handler bobkonf.handler/app}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [ring/ring-mock "0.3.0"]]}})
+                        [ring/ring-mock "0.3.0"]
+                        [cheshire "5.8.0"]
+                        [midje "1.9.1"]]
+         :plugins [[lein-midje "3.1.3"]]}})
